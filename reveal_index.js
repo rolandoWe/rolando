5af2ad19b260e8a715e050931978bdeb6e6d5ed7
Rolando <riverarolando71@gmail.com>
2021-02-02 16:22:18 -0600

    Add files via upload

---
+++ reveal_index.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+
+sr.reveal('.cinta', {
+   duration:2000,
+   origin:"top",
+   distance:"200px"
+});
+sr.reveal('.yo_img, .contenedor_lala', {
+   duration:4000,
+   origin:"left",
+   distance:"200px"
+});
+sr.reveal('.yo_texto', {
+   duration:3000,
+   origin:"right",
+   distance:"200px"
+});
+sr.reveal('.fecha, .secNoti_1', {
+   duration:3000,
+   origin:"bottom",
+   distance:"50px"
+});
+
+
